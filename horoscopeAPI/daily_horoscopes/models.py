3bb11f712aa76275b97eddf4d21f1be1333e825d
ivan-shumiln <shumilin.i.v@gmail.com>
2022-03-08 20:08:01 +0300

    add model Forecast

--- horoscopeAPI/daily_horoscopes/models.py
+++ horoscopeAPI/daily_horoscopes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Forecast(models.Model):
+    forecast = models.CharField('forecast', max_length=5000)
+
+    def __str__(self):
+        return self.forecast
--- horoscopeAPI/daily_horoscopes/views.py
+++ horoscopeAPI/daily_horoscopes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Forecast
 
 # Create your views here.
+def index(request):
+    """
+    Контроллер для отображения на главной странице списка всех записей.
+    """
+    list_of_forecast = Forecast.objects.all()
+    context = {'list_of_forecast': list_of_forecast}
+    return render(
+        request=request,
+        template_name='daily_horoscopes/index.html',
+        context=context
+    )
